367012ec43edd3a0c3efe71fe19837552113b185
Hilary Nelson <hilarynelson@Hilarys-MacBook-Pro.local>
2021-09-29 17:17:10 -0700

    added new task functionality

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
 
 let data = [
   {
-    id: 0,
+    id: 1,
     name: "test",
     check: false
   },
   {
-    id: 1,
+    id: 2,
     name: "test2",
     check: true
   }
@@ -23,7 +25,7 @@ const dropdownOptions = {
 const menuItems = [
   {
     id: "view",
-    name: "view"
+    name: "View"
   },
   {
     id: "trash",
@@ -31,44 +33,76 @@ const menuItems = [
   }
 ]
 
-function DropdownButton(props) {
-  return <div className="dropdown" id="view-button">
-    <button className="menu-buttons">{props.name}<span className="small-triangle"> ▼ </span></button>
-    <div className="dropdown-content">
-      <a href="#">{props.items.option1}</a>
-      <a href="#">{props.items.option2}</a>
-      <a href="#">{props.items.option3}</a>
-    </div>
-  </div>;
-}
+function App() {
+  const [tasks, setTasks] = useState(data);
+  const [checked, setChecked] = useState(tasks.filter(element => element.check === true));
 
-function ButtonsAndTasks(props) {
-  return <div className="buttons-and-tasks">
-    {props.buttonData.map(e => <DropdownButton name={e.name} items={menuItems}/>)}
-    <TaskContainer data={data}/>
-  </div>
+  function DeleteOrView(id, option) {
+    if (id === "trash") {
+      if (option === "All Tasks") {
+        setTasks([])
+      } else if (option === "Completed Tasks") {
+        setTasks(tasks.filter(element => element.check === true))
+      } else if (option === "Uncompleted Tasks") {
+        setTasks(tasks.filter(element => element.check === false))
+      }
+    }
   }
 
-function TaskContainer(props) {
-  return <div id="task-container">
-          {props.data.map( e => <TaskItem taskData = {e}/>)}
-        </div>;
-}
+  function DropdownButton(props) {
+    return <div className="dropdown" id="view-button">
+      <button className="menu-buttons">{props.name}<span className="small-triangle"> ▼ </span></button>
+      <div className="dropdown-content">
+        <button onClick={DeleteOrView(props.id, props.options.option1)}>{props.options.option1}</button>
+        <button onClick={DeleteOrView(props.id, props.options.option2)}>{props.options.option2}</button>
+        <button onClick={DeleteOrView(props.id, props.options.option3)}>{props.options.option3}</button>
+      </div>
+    </div>;
+  }
 
-function TaskItem(props) {
-  return <label className="task-item">
-            <input type="checkbox" className="check"/>
-              <span className="checkmark"></span>
-              {props.taskData.name}
-              <button className="edit" type="button">edit</button>
-          </label>;
-}
+  function MakeNewItem() {
+    setTasks([      {
+      id: generateUniqueID,
+      name: "",
+      checked: false
+    },
+      ...tasks])
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <ButtonsAndTasks buttonData={menuItems} items={dropdownOptions}/>
+  function ButtonsAndTasks(props) {
+    return <div className="buttons-and-tasks">
+      <div className="menu-buttons-container">
+        <div className="dropdown" id="new-item-button">
+          <button type="button" className="menu-buttons" onClick={MakeNewItem}>New Item</button>
+        </div>
+        {props.buttonData.map(e => <DropdownButton name={e.name} options={dropdownOptions}/>)}
+      </div>
+      <TaskContainer data={tasks}/>
     </div>
+  }
+
+  function TaskContainer(props) {
+    return <div id="task-container">
+      {tasks.map( e => <TaskItem taskData = {e}/>)}
+    </div>;
+  }
+
+  function TaskItem(props) {
+    return <label className="task-item">
+      <input type="checkbox" className="check"/>
+      <span className="checkmark"></span>
+      <span>{props.taskData.name}</span>
+      <button className="edit" type="button">edit</button>
+    </label>;
+  }
+
+
+  return (
+      <div className="App">
+        <div id="app-title"><h2>Tasks</h2>
+        </div>
+        <ButtonsAndTasks buttonData={menuItems}/>
+      </div>
   );
 }
 
