9873fbefea0d4669b3beeaeb29557fd3296886ca
Hilary Nelson <hilarynelson@Hilarys-MacBook-Pro.local>
2021-10-20 14:38:59 -0700

    added firebase

--- src/App.js
+++ src/App.js
@@ -4,21 +4,35 @@ import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
 import Alert from "./Alert";
 import DropdownButton from "./DropdownButton";
 import TaskContainer from "./TaskContainer";
+import {useCollection} from "react-firebase-hooks/firestore";
+import firebase from "firebase/compat";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCd9qqxvMpEKpBzwfWcc2tlRFa6ICaLH_s",
+  authDomain: "hmc-cs124-fa21-labs.firebaseapp.com",
+  projectId: "hmc-cs124-fa21-labs",
+  storageBucket: "hmc-cs124-fa21-labs.appspot.com",
+  messagingSenderId: "949410042946",
+  appId: "1:949410042946:web:0113b139a7e3cd1cc709db"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
 
 let currentDeleteOption = "";
 
-let data = [
-  {
-    id: 1,
-    name: "test",
-    checked: false
-  },
-  {
-    id: 2,
-    name: "test2",
-    checked: false
-  }
-]
+// let data = [
+//   {
+//     id: 1,
+//     name: "test",
+//     checked: false
+//   },
+//   {
+//     id: 2,
+//     name: "test2",
+//     checked: false
+//   }
+// ]
 
 const dropdownOptions = {
   option1: "All Tasks",
@@ -38,9 +52,17 @@ const menuItems = [
 ]
 
 function App() {
-  const [tasks, setTasks] = useState(data);
+  // const [tasks, setTasks] = useState(data);
   const [showAlert, setShowAlert] = useState(false);
   const [currView, setCurrView] = useState("All Tasks");
+
+  const collection = db.collection('hilnels-hmc-tasks');
+  const [value, loading, error] = useCollection(collection);
+  let data = []
+
+  if (value) {
+    data = value.docs.map(e => {return { ...e.data(), id: e.id }});
+  }
 
   function toggleModal() {
     setShowAlert(!showAlert);
@@ -51,27 +73,32 @@ function App() {
   }
 
   function handleTaskNameChange(e, id) {
-    console.log("handling task name change!")
-    let taskIndex = tasks.findIndex(e => e.id === id);
-    tasks[taskIndex].name = e.target.value
-    console.log(tasks)
-    setTasks(tasks);
+    // let taskIndex = tasks.findIndex(e => e.id === id);
+    // tasks[taskIndex].name = e.target.value
+    // console.log(tasks)
+    // setTasks(tasks);
+
+    collection.doc(id).update({name: e.target.value});
+
   }
 
   function toggleCheckbox(id) {
-    tasks.find(e => e.id === id).checked = !tasks.find(e => e.id === id).checked
+    console.log("in toggle checkbox")
+    const oldChecked = data.find(e => e.id === id).checked;
+    console.log(oldChecked);
+    collection.doc(id).update({checked: !oldChecked})
   }
 
   function deleteOrView(id, option) {
     if (id === "trash") {
       if (option === "All Tasks") {
-          setTasks([])
+          // setTasks([])
       } else if (option === "Completed Tasks") {
-          let newTasks = tasks.filter(element => element.checked === false)
-          setTasks(newTasks)
+          // let newTasks = tasks.filter(element => element.checked === false)
+          // setTasks(newTasks)
       } else if (option === "Uncompleted Tasks") {
-          let newTasks = tasks.filter(element => element.checked === true)
-          setTasks(newTasks)
+          // let newTasks = tasks.filter(element => element.checked === true)
+          // setTasks(newTasks)
       }
     }
     if (id === "view") {
@@ -85,46 +112,18 @@ function App() {
     }
   }
 
-  // function deleteOptions(id, option){
-  //   if (id === "trash") {
-  //     if (option === "All Tasks") {
-  //       setTasks([])
-  //     } else if (option === "Completed Tasks") {
-  //       let newTasks = tasks.filter(element => element.checked === false)
-  //       setTasks(newTasks)
-  //     } else if (option === "Uncompleted Tasks") {
-  //       let newTasks = tasks.filter(element => element.checked === true)
-  //       setTasks(newTasks)
-  //     }
-  //   }
-  // }
-  //
-  // function viewOptions(id, option) {
-  //   if (id === "view") {
-  //     if (option === "All Tasks") {
-  //       setCurrView("All Tasks")
-  //     } else if (option === "Completed Tasks") {
-  //       setCurrView("Completed Tasks")
-  //     } else if (option === "Uncompleted Tasks") {
-  //       setCurrView("Uncompleted Tasks")
-  //     }
-  //   }
-  // }
-
   function showTask(task) {
     return ((currView === "All Tasks") || (currView === "Completed Tasks" && task.checked) || (currView === "Uncompleted Tasks" && !task.checked))
   }
 
-  function MakeNewItem() {
-    setTasks([      {
-      id: generateUniqueID(),
-      name: "Click to Enter Task",
-      checked: false,
-    },
-      ...tasks]);
+  function makeNewItem() {
+    const newId = generateUniqueID();
+    collection.doc(newId).set({id: newId, name: "Click to Enter Task", checked: false})
   }
 
-  return (
+  return <div>
+      {
+        loading ? <div>Loading...</div> :
       <div className="App">
         <h2>Tasks</h2>
         {showAlert && <Alert onClose={toggleModal} onOK={() => deleteOrView("trash", currentDeleteOption)} dropdownOptions={dropdownOptions}>
@@ -135,14 +134,15 @@ function App() {
         <div className="buttons-and-tasks">
           <div className="menu-buttons-container">
             <div className="dropdown" id="new-item-button">
-              <button type="button" className="menu-buttons" onClick={MakeNewItem}>New Item</button>
+              <button type="button" className="menu-buttons" onClick={makeNewItem}>New Item</button>
             </div>
-            {menuItems.map(e => <DropdownButton key={e.id} setCurrentDeleteOption={setCurrentDeleteOption}  toggleModal={toggleModal} tasksData={tasks} {...e} options={dropdownOptions} deleteOrView={deleteOrView}/>)}
+            {menuItems.map(e => <DropdownButton key={e.id} setCurrentDeleteOption={setCurrentDeleteOption}  toggleModal={toggleModal} tasksData={data} {...e} options={dropdownOptions} deleteOrView={deleteOrView}/>)}
           </div>
-          <TaskContainer handleTaskNameChange={handleTaskNameChange} tasksData={tasks.filter(e => showTask(e))} toggleCheckbox={toggleCheckbox}/>
+          <TaskContainer handleTaskNameChange={handleTaskNameChange} tasksData={data} toggleCheckbox={toggleCheckbox}/>
         </div>
       </div>
-  );
+  }
+  </div>;
 }
 
 export default App;
