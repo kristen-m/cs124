bc2fa06969cf63ea519fa20d6b2e635d1a7ecf2b
kristenmason <kristen-m@users.noreply.github.com>
2021-10-31 17:21:13 -0700

    Fixed stuff

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import {useCollection} from "react-firebase-hooks/firestore";
 import firebase from "firebase/compat";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCd9qqxvMpEKpBzwfWcc2tlRFa6ICaLH_s",
-  authDomain: "hmc-cs124-fa21-labs.firebaseapp.com",
-  projectId: "hmc-cs124-fa21-labs",
-  storageBucket: "hmc-cs124-fa21-labs.appspot.com",
-  messagingSenderId: "949410042946",
-  appId: "1:949410042946:web:0113b139a7e3cd1cc709db"
+    apiKey: "AIzaSyCd9qqxvMpEKpBzwfWcc2tlRFa6ICaLH_s",
+    authDomain: "hmc-cs124-fa21-labs.firebaseapp.com",
+    projectId: "hmc-cs124-fa21-labs",
+    storageBucket: "hmc-cs124-fa21-labs.appspot.com",
+    messagingSenderId: "949410042946",
+    appId: "1:949410042946:web:0113b139a7e3cd1cc709db"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
@@ -22,158 +22,187 @@ const db = firebase.firestore();
 let currentDeleteOption = "";
 
 const dropdownOptions = {
-  option1: "All Tasks",
-  option2: "Completed Tasks",
-  option3: "Uncompleted Tasks"
+    option1: "All Tasks",
+    option2: "Completed Tasks",
+    option3: "Uncompleted Tasks"
 }
 
 const menuItems = [
-  {
-    id: "view",
-    name: "View"
-  },
-  {
-    id: "trash",
-    name: "🗑",
-  }
+    {
+        id: "view",
+        name: "View"
+    },
+    {
+        id: "trash",
+        name: "🗑",
+    }
 ]
 
 function App() {
-  const [sort, setSort] = useState("");
-  const [showAlert, setShowAlert] = useState(false);
-  const [currView, setCurrView] = useState("All Tasks");
+    const [sort, setSort] = useState("Date Created");
+    const [showAlert, setShowAlert] = useState(false);
+    const [currView, setCurrView] = useState("All Tasks");
 
-  let query = db.collection('hilnels-hmc-tasks');
-  const collection = db.collection('hilnels-hmc-tasks');
+    let query = db.collection('hilnels-hmc-tasks');
+    const collection = db.collection('hilnels-hmc-tasks');
 
-  if(currView === "All Tasks"){
-    query = db.collection('hilnels-hmc-tasks').orderBy("created", "asc");
-  } else if (currView === "Completed Tasks") {
-    query = db.collection('hilnels-hmc-tasks').where("checked", "==", true);
-  } else {
-    query = db.collection('hilnels-hmc-tasks').where("checked", "==", false);
-  }
-  const [value, loading, error] = useCollection(query);
-  let data = []
+    if (currView === "All Tasks") {
+        if (sort === "Name: A to Z") {
+            query = db.collection('hilnels-hmc-tasks').orderBy("name", "desc");
+        } else if (sort === "Name: Z to A") {
+            query = db.collection('hilnels-hmc-tasks').orderBy("name", "asc");
+        } else if (sort === "Priority: High to Low") {
+            query = db.collection('hilnels-hmc-tasks').orderBy("priority", "desc");
+        } else if (sort === "Priority: Low to High") {
+            query = db.collection('hilnels-hmc-tasks').orderBy("priority", "asc");
+        } else {
+            query = db.collection('hilnels-hmc-tasks').orderBy("created", "asc");
+        }
 
-  if (value) {
-    data = value.docs.map(e => {return { ...e.data(), id: e.id }});
-    data = data.reverse();
-  }
-
-  function toggleModal() {
-    setShowAlert(!showAlert);
-  }
-
-  function setCurrentDeleteOption(currDelete) {
-    currentDeleteOption = currDelete;
-  }
-
-  function handleTaskNameChange(e, id) {
-    // let taskIndex = tasks.findIndex(e => e.id === id);
-    // tasks[taskIndex].name = e.target.value
-    // console.log(tasks)
-    // setTasks(tasks);
-
-    collection.doc(id).update({name: e.target.value});
-
-  }
-
-  function toggleCheckbox(id) {
-    console.log("in toggle checkbox")
-    const oldChecked = data.find(e => e.id === id).checked;
-    console.log(oldChecked);
-    collection.doc(id).update({checked: !oldChecked})
-  }
-
-  function handleDeleteTasks(ids) {
-    ids.forEach(id => db.collection('hilnels-hmc-tasks').doc(id).delete());
-  }
-
-  function deleteOrView(id, option) {
-    if (id === "trash") {
-      if (option === "All Tasks") {
-        let ids = data.map(e => e.id);
-        handleDeleteTasks(ids);
-      } else if (option === "Completed Tasks") {
-        let ids = data.map(e => {if (e.checked) return e.id});
-        handleDeleteTasks(ids);
-      } else if (option === "Uncompleted Tasks") {
-        let ids = data.map(e => {if (!e.checked) return e.id});
-        handleDeleteTasks(ids);
-      }
+        query.orderBy("created", "asc");
+    } else if (currView === "Completed Tasks") {
+        query = db.collection('hilnels-hmc-tasks').where("checked", "==", true);
+    } else {
+        query = db.collection('hilnels-hmc-tasks').where("checked", "==", false);
     }
-    if (id === "view") {
-      if (option === "All Tasks") {
-        setCurrView("All Tasks")
-      } else if (option === "Completed Tasks") {
-        setCurrView("Completed Tasks")
-      } else if (option === "Uncompleted Tasks") {
-        setCurrView("Uncompleted Tasks")
-      }
+
+    const [value, loading, error] = useCollection(query);
+    let data = []
+
+    if (value) {
+        data = value.docs.map(e => {
+            return {...e.data(), id: e.id}
+        });
+        data = data.reverse();
     }
-  }
 
-  function showTask(task) {
-    return ((currView === "All Tasks") || (currView === "Completed Tasks" && task.checked) || (currView === "Uncompleted Tasks" && !task.checked))
-  }
+    function toggleModal() {
+        setShowAlert(!showAlert);
+    }
 
-  function updatePriority(id, priority) {
-    collection.doc(id).update( {priority: priority})
-  }
+    function setCurrentDeleteOption(currDelete) {
+        currentDeleteOption = currDelete;
+    }
 
-  function makeNewItem() {
-    const newId = generateUniqueID();
-    collection.doc(newId).set({id: newId,
-                             name: "Click to Enter Task",
-                          checked: false,
-                         priority: "High",
-                          created: firebase.database.ServerValue.TIMESTAMP})
-  }
+    function handleTaskNameChange(e, id) {
+        // let taskIndex = tasks.findIndex(e => e.id === id);
+        // tasks[taskIndex].name = e.target.value
+        // console.log(tasks)
+        // setTasks(tasks);
 
-  function getNewSort() {
-    console.log("In get new sort: "+document.getElementById("task-sorting").value);
-    setSort(document.getElementById("task-sorting").value);
-    console.log("Sort in get new sort: "+sort);
-  }
+        collection.doc(id).update({name: e.target.value});
 
-  return <div>
-      {
-        loading ? <div>Loading...</div> :
-      <div className="App">
-        <h2>Tasks</h2>
-        {showAlert && <Alert onClose={toggleModal} onOK={() => deleteOrView("trash", currentDeleteOption)} dropdownOptions={dropdownOptions}>
-          <div>
-            Are you sure you want to delete these tasks?
-          </div>
-        </Alert>}
-        <div className="buttons-and-tasks">
-          <div className="menu-buttons-container">
-            <div className="dropdown" id="new-item-button">
-              <button type="button" className="menu-buttons" onClick={makeNewItem}>New Item</button>
-            </div>
-            {menuItems.map(e => <DropdownButton key={e.id} setCurrentDeleteOption={setCurrentDeleteOption}  toggleModal={toggleModal} tasksData={data} {...e} options={dropdownOptions} deleteOrView={deleteOrView}/>)}
-          </div>
-          <div id="sorting-area">
-          <span>
-              <select name="sorting" id="task-sorting" onChange={() =>{
-                document.getElementById("task-sorting").addEventListener("click", getNewSort);
-                getNewSort();
-                console.log("Val of Sort: "+sort);}}>
-                <option selected hidden>Sort By:</option>
-                <option value="Date Created">Date Created</option>
-                <option value="Name: A to Z">Name: A to Z</option>
-                <option value="Name: Z to A">Name: Z to A</option>
-                <option value="Priority: High to Low">Priority: High to Low</option>
-                <option value="Priority: Low to High">Priority: Low to High</option>
-            </select>
-        </span>
-          </div>
-          <TaskContainer handleTaskNameChange={handleTaskNameChange} tasksData={data} toggleCheckbox={toggleCheckbox} updatePriority={updatePriority}/>
-        </div>
-      </div>
-  }
-  </div>;
+    }
+
+    function toggleCheckbox(id) {
+        console.log("in toggle checkbox")
+        const oldChecked = data.find(e => e.id === id).checked;
+        console.log(oldChecked);
+        collection.doc(id).update({checked: !oldChecked})
+    }
+
+    function handleDeleteTasks(ids) {
+        ids.forEach(id => db.collection('hilnels-hmc-tasks').doc(id).delete());
+    }
+
+    function deleteOrView(id, option) {
+        if (id === "trash") {
+            if (option === "All Tasks") {
+                let ids = data.map(e => e.id);
+                handleDeleteTasks(ids);
+            } else if (option === "Completed Tasks") {
+                let ids = data.map(e => {
+                    if (e.checked) return e.id
+                });
+                handleDeleteTasks(ids);
+            } else if (option === "Uncompleted Tasks") {
+                let ids = data.map(e => {
+                    if (!e.checked) return e.id
+                });
+                handleDeleteTasks(ids);
+            }
+        }
+        if (id === "view") {
+            if (option === "All Tasks") {
+                setCurrView("All Tasks")
+            } else if (option === "Completed Tasks") {
+                setCurrView("Completed Tasks")
+            } else if (option === "Uncompleted Tasks") {
+                setCurrView("Uncompleted Tasks")
+            }
+        }
+    }
+
+    function showTask(task) {
+        return ((currView === "All Tasks") || (currView === "Completed Tasks" && task.checked) || (currView === "Uncompleted Tasks" && !task.checked))
+    }
+
+    function updatePriority(id, priority) {
+        collection.doc(id).update({priority: priority})
+    }
+
+    // a is highest priority
+    function makeNewItem() {
+        const newId = generateUniqueID();
+        collection.doc(newId).set({
+            id: newId,
+            name: "Click to Enter Task",
+            checked: false,
+            priority: "c",
+            created: firebase.database.ServerValue.TIMESTAMP
+        })
+    }
+
+    function getNewSort() {
+        console.log("In get new sort: " + document.getElementById("task-sorting").value);
+        setSort(document.getElementById("task-sorting").value);
+        console.log("Sort in get new sort: " + sort);
+    }
+
+    return <div>
+        {
+            loading ? <div>Loading...</div> :
+                <div className="App">
+                    <h2>Tasks</h2>
+                    {showAlert && <Alert onClose={toggleModal} onOK={() => deleteOrView("trash", currentDeleteOption)}
+                                         dropdownOptions={dropdownOptions}>
+                        <div>
+                            Are you sure you want to delete these tasks?
+                        </div>
+                    </Alert>}
+                    <div className="buttons-and-tasks">
+                        <div className="menu-buttons-container">
+                            <div className="dropdown" id="new-item-button">
+                                <button type="button" className="menu-buttons" onClick={makeNewItem}>New Item</button>
+                            </div>
+                            {menuItems.map(e => <DropdownButton key={e.id}
+                                                                setCurrentDeleteOption={setCurrentDeleteOption}
+                                                                toggleModal={toggleModal} tasksData={data} {...e}
+                                                                options={dropdownOptions}
+                                                                deleteOrView={deleteOrView}/>)}
+                        </div>
+                        {(currView === "All Tasks") && <div id="sorting-area">
+<span>
+
+<div id="sort">
+<div id="sort-label">Sort By:</div>
+<select name="sorting" id="task-sorting" onChange={e => setSort(e.target.value)}>
+<option selected hidden>Sort By:</option>
+<option value="Date Created" selected={"Date Created" === sort}>Date Created</option>
+<option value="Name: A to Z" selected={"Name: A to Z" === sort}>Name: A to Z</option>
+<option value="Name: Z to A" selected={"Name: Z to A" === sort}>Name: Z to A</option>
+<option value="Priority: High to Low" selected={"Priority: High to Low" === sort}>Priority: High to Low</option>
+<option value="Priority: Low to High" selected={"Priority: Low to High" === sort}>Priority: Low to High</option>
+</select>
+</div>
+</span>
+                        </div>}
+                        <TaskContainer handleTaskNameChange={handleTaskNameChange} tasksData={data}
+                                       toggleCheckbox={toggleCheckbox} updatePriority={updatePriority}/>
+                    </div>
+                </div>
+        }
+    </div>;
 }
 
 export default App;
--- src/TaskItem.js
+++ src/TaskItem.js
@@ -1,13 +1,13 @@
 function TaskItem(props) {
     return <label className="task-item" >
-        <div>
+        <div id={"task-color-" + props.priority}>
             <input type="checkbox" className="check" defaultChecked={props.checked} onClick={() => props.toggleCheckbox(props.id)}/>
             <span className="checkmark"></span>
             <input defaultValue={props.name} onChange={e => props.handleTaskNameChange(e, props.id)}></input>
             <select name="Priority" id="priority" onChange={e => props.updatePriority(props.id, e.target.value)}>
-                <option value="High" selected= {"High"===props.priority}>High</option>
-                <option value="Med" selected= {"Med"===props.priority}>Med</option>
-                <option value="Low" selected= {"Low"===props.priority}>Low</option>
+                <option value="a" selected= {"a"===props.priority}>High</option>
+                <option value="b" selected= {"b"===props.priority}>Med</option>
+                <option value="c" selected= {"c"===props.priority}>Low</option>
             </select>
         </div>
     </label>;
