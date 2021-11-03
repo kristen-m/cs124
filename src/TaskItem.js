8db4c9cd6debcb8f22f8892944aa69c3f69794ad
hilnels <59988743+hilnels@users.noreply.github.com>
2021-11-03 10:26:57 -0700

    Update TaskItem.js
    multiline editing for tasks update

--- src/TaskItem.js
+++ src/TaskItem.js
@@ -1,10 +1,21 @@
+import TextField from '@mui/material/TextField';
+
 function TaskItem(props) {
     return <label className="task-item" >
         <div id={"task-color-" + props.priority + "-" + props.checked}
         >
             <input type="checkbox" className="check" defaultChecked={props.checked} onClick={() => props.toggleCheckbox(props.id)}/>
             <span className="checkmark"></span>
-            <input value={props.name} disabled={props.checked} placeholder="Click to Enter Task" onChange={e => props.handleTaskNameChange(e, props.id)}></input>
+            {/*<input value={props.name} disabled={props.checked} placeholder="Click to Enter Task" onChange={e => props.handleTaskNameChange(e, props.id)}></input>*/}
+            <TextField
+                id="task-text-entry"
+                placeholder="Click to Enter Task"
+                value={props.name}
+                onChange={e => props.handleTaskNameChange(e, props.id)}
+                disabled={props.checked}
+                InputProps={{ style: { fontSize: 40} }}
+                multiline
+            />
             <select name="Priority" id="priority" onChange={e => props.updatePriority(props.id, e.target.value)}>
                 <option value="a" selected= {"a"===props.priority}>High</option>
                 <option value="b" selected= {"b"===props.priority}>Med</option>
